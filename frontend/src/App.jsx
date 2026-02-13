499a1365391fd7481e5a350a8cb5ac3a65734243
SergioRafa <sergiodeoliveirarafael@gmail.com>
2026-02-13 15:28:52 -0300

    feat: organiza componentes da Home e configura rotas da aplicação

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,8 +6,14 @@ export default function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* Raiz do site: O Cadastro do Médico */}
         <Route path="/" element={<Home />} />
+        
+        {/* Área Protegida: O Simulador Cirúrgico */}
         <Route path="/simulator" element={<Simulator />} />
+        
+        {/* Rota de escape: Se digitar qualquer coisa errada, volta pra Home */}
+        <Route path="*" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
