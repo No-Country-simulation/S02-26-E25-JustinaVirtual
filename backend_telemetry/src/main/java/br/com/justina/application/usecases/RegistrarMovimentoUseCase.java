4becfa91f0711f06be09d1b7ffb0c0be4b022b3a
DevFabJava <dev.fabioalmeida@proton.me>
2026-02-13 11:52:26 -0300

    Salvando progresso antes de atualizar com a dev

--- backend_telemetry/src/main/java/br/com/justina/application/usecases/RegistrarMovimentoUseCase.java
+++ backend_telemetry/src/main/java/br/com/justina/application/usecases/RegistrarMovimentoUseCase.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @Service
 @RequiredArgsConstructor
-public class ProcessarTelemetriaUseCase {
+public class RegistrarMovimentoUseCase {
 
     private final ITelemetriaRepositoryPort repository;
     private final IAiClientPort aiClient;
