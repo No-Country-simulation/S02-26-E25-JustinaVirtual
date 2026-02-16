4faa4c668f18b506b106c0a74ee16281b91fcea1
SergioRafa <sergiodeoliveirarafael@gmail.com>
2026-02-16 17:12:06 -0300

    fix: adicionado TrainingSessionProvider ao ponto de entrada da aplicação

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,17 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { AuthProvider } from "./contexts/AuthContext";
+// 1. Importe o Provider 
+import { TrainingSessionProvider } from "./contexts/TrainingSessionContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <AuthProvider>
-    <App />
-  </AuthProvider>
+  <React.StrictMode>
+    <AuthProvider>
+      {/* 2. Envolva a aplicação com o TrainingSessionProvider */}
+      <TrainingSessionProvider>
+        <App />
+      </TrainingSessionProvider>
+    </AuthProvider>
+  </React.StrictMode>
 );
