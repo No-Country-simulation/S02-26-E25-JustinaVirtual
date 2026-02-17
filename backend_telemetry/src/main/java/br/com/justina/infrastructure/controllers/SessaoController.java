fecc5fd02fc545d5399c93d81302e34ff4952e69
Stephanny Soares <stephannysoares.3@hotmail.com>
2026-02-17 19:42:16 +0100

    feat: implementa endpoint de historico de sessoes (#20)

--- backend_telemetry/src/main/java/br/com/justina/infrastructure/controllers/SessaoController.java
+++ backend_telemetry/src/main/java/br/com/justina/infrastructure/controllers/SessaoController.java
@@ -4,11 +4,14 @@ import br.com.justina.application.dto.FinalizarSessaoRequest;
 import br.com.justina.application.dto.SessaoResponse;
 import br.com.justina.application.usecases.AbrirSessaoUseCase;
 import br.com.justina.application.usecases.FinalizarSessaoUseCase;
+import br.com.justina.application.usecases.ListarHistoricoUseCase;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -19,6 +22,7 @@ public class SessaoController {
 
     private final AbrirSessaoUseCase abrirSessaoUseCase;
     private final FinalizarSessaoUseCase finalizarSessaoUseCase;
+    private final ListarHistoricoUseCase listarHistoricoUseCase;
 
     @PostMapping("/iniciar")
     public ResponseEntity<SessaoResponse> iniciarSessao() {
@@ -32,5 +36,10 @@ public class SessaoController {
             @RequestBody @Valid FinalizarSessaoRequest request
     ) {
         return ResponseEntity.ok(finalizarSessaoUseCase.executar(id, request));
+    }
+
+    @GetMapping("/historico")
+    public ResponseEntity<List<SessaoResponse>> obterHistorico() {
+        return ResponseEntity.ok(listarHistoricoUseCase.executar());
     }
 }
